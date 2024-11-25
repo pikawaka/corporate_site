f8b02bfa62aefff7d31700ec6acdf26408bdcbcd
荒木　真 <lineinfo@infoseek.jp>
2024-11-25 10:32:35 +0900

    アニメーションの追加と変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.js-menu-toggle');
   const nav = document.querySelector('.c-nav');
   const overlay = document.querySelector('.c-overlay');
+  const navLinks = document.querySelectorAll('.c-nav__link');
 
   menuToggle.addEventListener('click', function() {
     nav.style.display = 'flex';
@@ -16,15 +17,42 @@ document.addEventListener('DOMContentLoaded', function() {
     nav.style.display = 'none';
     overlay.style.display = 'none';
   });
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      if (window.getComputedStyle(menuToggle).display !== 'none') {
+        nav.style.display = 'none';
+        overlay.style.display = 'none';
+      }
+    });
+  });
 });
 
 
 /*--------------------------------------*
- * トップページのナビクリック時のアニメーション
+ * トップページのヘッダーナビクリック時のアニメーション
  *--------------------------------------*/ 
 document.addEventListener('DOMContentLoaded', function () {
 
   document.querySelectorAll('.c-nav__link--index').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
+});
+
+/*--------------------------------------*
+ * トップページのフッターナビクリック時のアニメーション
+ *--------------------------------------*/ 
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelectorAll('.l-footer__nav-link').forEach(link => {
     link.addEventListener('click', function (e) {
       e.preventDefault();
       const targetId = this.getAttribute('href').substring(1);
